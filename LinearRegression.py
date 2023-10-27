c96e2d92602e1f9dc476523b3e90159981515a53
DarthVader118 <63321893+DarthVader118@users.noreply.github.com>
2023-10-26 21:17:48 -0400

    first

---
+++ LinearRegression.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 20:12:12 2023
+
+@author: Raghav Goel
+"""
+
+class LinearRegression:
+    
+    def __init__():
---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 20:58:58 2023
+
+@author: Raghav Goel
+"""
+
+import LinearRegression as lr
+
+lr = LinearRegression()
