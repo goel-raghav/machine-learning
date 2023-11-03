412345633702d8d1646aa77766268521d41312b1
goel-raghav <goelr2007@gmail.com>
2023-11-02 23:07:53 -0400

    implemented gradient descent

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,10 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 26 20:12:12 2023
+import numpy as np
+import matplotlib.pyplot
 
-@author: Raghav Goel
-"""
-
-class LinearRegression:
-    
-    def __init__():
+class Model:
+    def __init__(self, X: np.ndarray, W: np.ndarray, Y: np.ndarray):
+        self.X = X
+        ones = np.ones((X.shape[0], 1))
+        self.X = np.column_stack((ones, self.X))
+        
+        self.W = W
+        self.Y = Y
+        
+    def fit(self, max_iter: int = 1000, learning_rate: float = 0.01) -> None:
+        finished = False
+        count = 0
+        
+        while not finished and count < max_iter:
+            descent = self._calculate_descent()
+            self.W -= learning_rate * descent
+            count += 1
+            
+            if np.linalg.norm(descent) < 0.00001:
+                finished = True
+            
+        return count
+        
+    def _calculate_descent(self):
+        Y_pred = np.dot(self.X, self.W)
+        difference = (Y_pred - self.Y).reshape(-1, 1)
+        descent = self.X * difference
+        descent = np.mean(descent, axis = 0)
+        return descent
--- test.py
+++ test.py
@@ -4,7 +4,17 @@ Created on Thu Oct 26 20:58:58 2023
 
 @author: Raghav Goel
 """
+import numpy as np
+import LinearRegression
 
-import LinearRegression as lr
+x = np.arange(10.0).reshape(10, 1)
+m = 3
+b = 9
+y = (x * m + b).reshape(-1, )
+w = np.zeros(x.shape[-1] + 1)
 
-lr = LinearRegression()
+lr = LinearRegression.Model(x, w, y) 
+iterations = lr.fit(10000)
+
+print(lr.W)
+print(iterations)
